4e3617fce9f891961dd673fbe825e298eda7c999
Nasser <n.alneama@gmail.com>
2016-01-13 10:22:59 +0300

    Added Fragment container

--- app/src/main/java/com/nasserapps/learningandroid/MainActivity.java
+++ app/src/main/java/com/nasserapps/learningandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nasserapps.learningandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if(fragment==null){
+            fragment= new BasicButtonFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container,fragment)
+                    .commit();
+        }
+
     }
 }
